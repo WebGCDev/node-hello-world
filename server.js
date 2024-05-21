6cd666f1d45b4aeac11083adbde439f6e8949f12
WebGCDev <gaetano.chi@gmail.com>
2024-05-21 16:01:39 +0200

    Add: .Env & gitignore Loading Ok

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
 const http = require('http');
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || 'localhost';
+const messaggio = process.env.MY_VARIABLE || 'Primo esercizio,buon corso!';
 http
   .createServer(function (req, res) {
     res.writeHead(200, {
       'Content-Type': 'text/html',
     });
-    res.end('Primo esercizio Corso Intensivo React!');
+    res.end(`<h1>${messaggio}</h1>`);
   })
   .listen(port, host, () => {
     const serverUrl = `http://${host}:${port}`;
